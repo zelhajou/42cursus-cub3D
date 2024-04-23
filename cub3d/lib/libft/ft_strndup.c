3bbde63e30a6e46be8b5e7fe41ba8d3fb8c44da3
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-04-23 11:09:07 +0100

    add libft library and update the makefile

---
+++ cub3d/lib/libft/ft_strndup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 16:13:17 by zelhajou          #+#    #+#             */
+/*   Updated: 2024/02/14 16:15:37 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strndup(const char *src, size_t n)
+{
+	size_t	i;
+	size_t	size;
+	char	*str;
+
+	i = 0;
+	size = ft_strnlen(src, n);
+	str = malloc(size + 1);
+	if (str == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
